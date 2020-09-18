e4703c3b705217df788c8539c178c60d38ab789b
anirudh <anirudhsharma8303@gmail.com>
2020-09-18 22:00:25 +0530

    Firebse Changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 const firebaseConfig = {
-    apiKey: "AIzaSyDOeRrWRlG2G2zm7OcWJnt6wt4Vnn3bd-I",
-    authDomain: "nidar-6ce3c.firebaseapp.com",
-    databaseURL: "https://nidar-6ce3c.firebaseio.com",
-    projectId: "nidar-6ce3c",
-    storageBucket: "nidar-6ce3c.appspot.com",
-    messagingSenderId: "239921622288",
-    appId: "1:239921622288:web:d5a36961c452e0e67f4995",
-    measurementId: "G-N1LC6Z95S8"
+    apiKey: "AIzaSyBo5uROT2glg2equWDJFDRChiEPQTCdPFE",
+    authDomain: "nirdar-1b9e0.firebaseapp.com",
+    databaseURL: "https://nirdar-1b9e0.firebaseio.com",
+    projectId: "nirdar-1b9e0",
+    storageBucket: "nirdar-1b9e0.appspot.com",
+    messagingSenderId: "97521617366",
+    appId: "1:97521617366:web:c7fa0c03f225ef46ba3c77",
+    measurementId: "G-MV3JCV522V"
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
